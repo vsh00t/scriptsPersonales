3ebfe1c526f039e4b09cbaac961430296336cea5
virtualshoot <37148657+virtualshoot@users.noreply.github.com>
2018-03-22 08:25:28 -0500

    Update borradoIndices.sh

--- borradoIndices.sh
+++ borradoIndices.sh
@@ -6,8 +6,4 @@ do
     curl -XDELETE localhost:9200/netflow-$fecha
     echo "borrando indices antiguos netflow-$fecha" >> /var/log/borrado.log
 done
-proceso=`ps -u | grep 9996 | grep -v grep| awk '{print $2}'`
-kill -9 $proceso
-systemctl restart elasticsearch
-systemctl restart logstash
-nohup /usr/share/logstash/bin/logstash --modules netflow -M netflow.var.input.udp.port=9996 > /var/log/netflow.log &
+
